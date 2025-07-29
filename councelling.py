a319538b895216369f913331e32a6e05ce1c43be
satwika <satwika.pandillapalli2836@gmail.com>
2025-07-29 23:15:03 +0530

    councellinggitdemo

---
+++ councelling.py
@@ -0,0 +1,169 @@
+<html>
+<head>
+<title> concelling form </title>
+</head>
+<body>
+<table align="center" border=3 width=80% cellpadding="8">
+<img src="gvp logo.JPG" width="140" height="130" align="left" style="padding-left: 140px">
+<center>
+<font face="brush script" size="5" color="black"><b>GAYATRI VIDYA PARISHAD COLLEGE OF ENGINEERING FOR WOMEN(A)</b></font><br>
+<font face="brush script" size="4" color="black"><b>Madhurawada :: Visakhapatnam - 530 048</b></font><br>
+<font face="brush script" size="3" color="black"><b>(Approved by AICTE,New Delhi Permanently Affiliated to Andhra University,Visakhapatnam)</b></font><br>
+<font face="brush script" size="5" color="black"><b>Accredited by NAAC with "A" from 2022 to 2027</b></font><br>
+<font face="brush script" size="4" color="black"><b>CSE,ECE and IT Courses Accredited by NBA(2019-2022) and Re-accredited by NBA(2022-2025)</b></font><br>
+<font face="brush script" size="4" color="black"><b>EEE Course Accredited by NBA(2023-2026)</b></font><br>
+<font face="brush script" size="2" color="black"><b>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;ph  : 0891-2739144      Fax : 0891-2526639   e-mail:<u>gvpcew@gmail.com</u>    Website: <u>www.gvpcew.ac.in</u></b></font><br>
+<h2><u><b>STUDENT PROFILE</b></u></h2></center>
+<tr>
+<th>HT NO.</th>
+<th> NAME OF THE STUDENT(as per SSC)</th>
+<th> BRANCH</th></tr>
+</tr>
+<tr>
+<td align="center"><input type="text" size="50"></input></td>
+<td align="center"><input type="text" size="50"></input></td>
+<td align="center"><input type="text" size="50"></input></td></tr>
+</table><br>
+<table align="center" border=3 width="80%" cellpadding="8">
+<tr>
+<th>Date of Birth
+(as per SSC)</th>
+<th> AADHAR No.(self)</th>
+<th> Mobile no.(self)</th>
+<th> e-mail ID</th>
+</tr>
+</tr>
+<tr>
+<td align="center"><input type="text" size="35"></input></td>
+<td align="center"><input type="text" size="35"></input></td>
+<td align="center"><input type="text" size="35"></input></td>
+<td align="center"><input type="text" size="35"></input></td>
+</tr>
+</table><br>
+<table align="center" border=3 width="80%" cellpadding="4">
+<tr>
+<td colspan=50%><center><b>Educational Details<b></center></td></tr>
+<tr>
+<td colspan=4><b><center>SSC/CBSE etc</center></b></td>
+<td colspan=4><b><center> School of Study & Place</center></b></td></tr>
+<tr>
+<td >% of Marks</td><td align="center"><input type="text"></input></td>
+<td>GRADE</td><td align="center"><input type="text"></input></td>
+<td align="center"><input type="text" size=100%></input></td></tr>
+<br>
+<tr>
+<td colspan=4><b><center>INTERMEDIATE/+2/DIPLOMA</center></b></td>
+<td colspan=4><b><center>College of Study & Place</center></b></td></tr>
+<tr>
+<td >% of Marks</td><td align="center"><input type="text"></input></td>
+<td>GRADE</td><td align="center"><input type="text" name="dob"></input></td>
+<td align="center"><input type="text" size=100%></input></td>
+</tr>
+</table><br>
+<table align="center" border=3 width="80%" cellpadding="5">
+<tr>
+<th colspan=20%>Qualifying Examination Details</th></tr>
+<tr><th colspan=3>EAMCET</th>
+<th colspan=3>JEE</th>
+<th colspan=3>ECET(Diploma)</th>
+</tr>
+<tr><td align="center">HT NO</td>
+<td align="center">YEAR</td>
+<td align="center">RANK</td>
+<td align="center">HT NO</td>
+<td align="center">YEAR</td>
+<td align="center">RANK</td>
+<td align="center">HT NO</td>
+<td align="center">YEAR</td>
+<td align="center">RANK</td>
+</tr>
+<tr>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+
+</tr>
+</table><br>
+<table align="center" border=3 width=80% cellpadding="8">
+<tr>
+<th colspan="7">Admission Details</th></tr><tr>
+<th colspan="4">Category of Admission</th>
+<th><b>Nationality & Religion</b></th>
+<th><b>Caste & Category</b></th>
+<th>State to which belongs to</th></tr>
+<tr><th colspan=2 align="center">EAMCET</th><th align="center">SPOT</th><th align="center">ECET</th><th rowspan=2><input type="text" size="30"></input>
+</th><th rowspan=2><input type="text" size="30"></input></th><th rowspan=2>Example-AP  <input type="text" size="10"></input>
+</th></tr>
+<tr><td colspan=1>Cat A</td><td colspan=1>Cat B</td>
+<td align="center"><input type="text" size="10"></input></td>
+<td align="center"><input type="text" size="10"></input></td>
+</tr>
+</table><br>
+<table align="center" border=3 width=80% cellpadding="8">
+<tr>
+<th>Availing AP Govt.Fee Reimbursement Scheme</th>
+<th>YES</th><th><input type="text" size="20"></input></th>
+<th>NO</th><th><input type="text" size="20"></input></th>
+<th>EWS</th><th><input type="text" size="20"></input></th>
+</tr>
+</table><br>
+<table align="center" border=3 width=80% cellpadding="8">
+<tr><th colspan="100%">Details of Parents</th></tr>
+<tr><th colspan="50%" align="center">Father's Name</th><th colspan="50%" align="center">Mother's Name</th></tr>
+<tr><td colspan="50%" align="center"><input type="text" size="50"></input>
+</td><td colspan="50%" align="center"><input type="text" size="50"></input>
+</td></tr>
+<tr><th colspan="25%">Qualification</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Qualification</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="25%">Occupation</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Occupation</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="25%">Annual Income</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Annual Income</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="25%">Land Line(Off/Res)</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Land Line(Off/Res)</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="25%">Mobile No.</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Mobile No.</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="25%">e-mail ID</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">e-mail ID</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="50%" align="center">PERMANENT ADDRESS</th><th colspan="50%" align="center">CONTACT ADDRESS</th></tr>
+<tr><th colspan="50%" align="center"><textarea rows="5" cols="30"></textarea></th><td colspan="50%" align="center"><textarea rows="5" cols="30"></textarea></td></tr></table>
+<br>
+<table align="center" border=3 width=80% cellpadding="8">
+<tr><th colspan="50%" align="center">DETAILS OF HOSTEL (if any)</th><th colspan="50%" align="center">Details of LOCAL Guardian</th></tr>
+<tr><th colspan="25%">College/Govt/Outside Hostel(Name)</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Name</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="25%">Address</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Address</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="25%">Phone No.</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Phone no.</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+<tr><th colspan="25%">Mobile no.</th><td colspan="25%" align="center"><input type="text" size="50"></input></td>
+<th colspan="25%">Mobile no.</th><td colspan="25%" align="center"><input type="text" size="50"></input></td></tr>
+</table>
+<br>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ time table.py
@@ -0,0 +1,80 @@
+<html>
+<head>
+<title>Time Table </title>
+</head><body>
+<table height=65% width=100% border=3>
+<img src="gvp logo.JPG" align="left"  width="130" height="130"><center><font face="brush script" size="5" color="black"><b>GAYATRI VIDYA PARISHAD COLLEGE OF ENGINEERING FOR WOMEN(A)</b></font><br>
+<font face="brush script" size="4" color="black"><b>Madhurawada :: Visakhapatnam - 530 048</b></font><br>
+<font face="brush script" size="3" color="black"><b>(Approved by AICTE,New Delhi Permanently Affiliated to Andhra University,Visakhapatnam)</b></font><br>
+<font face="brush script" size="5" color="black"><b>Accredited by NAAC with "A" from 2022 to 2027</b></font><br>
+<font face="brush script" size="4" color="black"><b>CSE,ECE and IT Courses Accredited by NBA(2019-2022) and Re-accredited by NBA(2022-2025)</b></font><br>
+<font face="brush script" size="4" color="black"><b>EEE Course Accredited by NBA(2023-2026)</b></font><br>
+<font face="brush script" size="2" color="black"><b>ph  : 0891-2739144      Fax : 0891-2526639   e-mail:<u>gvpcew@gmail.com</u>    Website: <u>www.gvpcew.ac.in</u> </b></font></center><br>
+
+<tr>
+<th>2csm2</th>
+<th>9:30AM-10:20AM</th>
+<th>10:20AM-11:10AM</th>
+<th>11:10AM-12:00PM</th>
+<th>12:00PM-12:50PM</th>
+<th>12:50PM-1:40PM</th>
+<th>1:40PM-2:30PM</th>
+<th>2:30PM-3:20PM</th>
+<th>3:20PM-4:10PM</th>
+<th>4:10PM-5:00PM</th>
+</tr>
+<tr>
+<th>MON</th>
+<td>DMS<br>(Mrs.DSSR)</td>
+<td>DS<br>(Dr.CS)</td>
+<td>DBMS<br>(Mr.PS)</td>
+<td>LUNCH</td>
+<td>OPPS TH JAVA<br>(Ms.NSM)</td>
+<td>DEV OPS<br>(Dr.KPN)</td>
+<td>PE&UHV<br>(Dr.RPN)</td>
+<td>NSS<br>(Mrs.PHL)</td>
+<td>AIMERS CLUB ACTIVITY</td>
+</tr>
+<tr>
+<th>TUE</th>
+<td>DMS<br>(Mrs.DSSR)</td>
+<td>DBMS<br>(Mr.PS)</td>
+<td>DS<br>(Dr.CS)</td>
+<td>LUNCH</td>
+<td colspan=3 align=center>OPPS TH JAVA LAB<br>(Ms.NSM,Mrs.HG)</td>
+<td>ME<br>(Mrs.KVK)</td>
+<td>LIBRARY</td>
+</tr>
+<tr>
+<th>WED</th>
+<td colspan=3 align=center>DS LAB<br>(Mrs.MD,Ms.NSM)</td>
+<td>LUNCH</td>
+<td>DMS<br>(Mrs.DSSR)</td>
+<td colspan=2 align=center>OOPS TH JAVA<br>(Ms.NSM)</td>
+<td>NSS<br>(Mrs.PHL)</td>
+<td>ME<br>(Mrs.KVK)</td>
+</tr>
+<tr>
+<th>THUR</th>
+<td colspan=3 align=center>DEVOPS LAB<br>(DR.KPN)</td>
+<td>LUNCH</td>
+<td>DS<br>(Dr.CS)</td>
+<td>OPPS TH JAVA<br>(Ms.NSM)</td>
+<td>ME<br>(Mrs.KVK)</td>
+<td>PE&UHV<br>(Dr.RPN)</td>
+<td>LIBRARY</td>
+</tr>
+<tr>
+<th>FRI</th>
+<td colspan=3 align=center>DBMS LAB(Mrs.PS,Mrs.PSD)</td>
+<td>LUNCH</td>
+<td>DS<br>(Dr.CS)</td>
+<td>DMS<br>(Mrs.DSSR)</td>
+<td>ME<br>(Mrs.KVK)</td>
+<td colspan=2 align=center>DBMS<br>(Mr.PS)</td>
+</tr></table>
+<p>DataBase ManagementSystems: Mr.P.SivaData Structures: Dr.C.Vasu, ObjectOriented Progrmming through JAVA: Ms.N.SaiMeghana Managerial Economics: Mrs.K. ValliKrishnaveni Discrete Mathematical Structures: Mrs.D.S.SivaRanjani Data Structures Lab:Dr.M.deephi, Ms.N.SaiMeghana Object Oriented Programming Through Java Lab: Ms.N.SaiMeghana Mrs.P.Sridevi, Database Management Systems Lab: Mrs.H.Gouthami, Mrs.P.Sridevi, Skill Development Course-I:DevOps: Dr.K.PurushothamNaidu, Professional Ethics & Universal Human Values: Dr.Rani PravaNanda,NCC/NSS:Ms.P.Hemalatha</p>
+</body>
+</html>
+
+
